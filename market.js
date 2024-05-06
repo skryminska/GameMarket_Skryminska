a1e3634aee5340c4775ccca4f62088fcc99a1657
skryminska <158287265+skryminska@users.noreply.github.com>
2024-05-06 15:26:57 +0300

    ADD

--- market.js
+++ market.js
@@ -1,29 +1,40 @@
 
-//перевірка 
-console.log('Певірка підключення файлу скриптів market.js')
 
-//отримання елементу з id items
-let itemsDiv = document.getElementById("items");
+//перевірка підключеного файлу скриптів
+console.log('Перевірка підключеного файлу скрипту market.js')
 
-//перевірка на існування знайденого блоку
-if(itemsDiv){
-    //вивід знайденого елементу
+//Отримання елементу з ідентифікатором items
+let itemsDiv = document.getElementById("items")
+
+//Вивід знайдкного елементу
+console.log(itemsDiv)
+
+//Перевірка існування знайденого блоку
+if (itemsDiv) {
+    //Вивід знайденого елементу
     console.log(itemsDiv)
 
-    itemsDiv.innerHTML = '<div class = "item"></div>'
-    //вивід значення поля 
-    //console.log('Поле classlist:', itemsDiv.classList)
-    //console.log('Поле id:',itemsDiv.id)
-    //console.log('Поле clientWidth:', itemsDiv.clientWidth)
-    //console.log('Поле innerHTML:', itemsDiv.innerHTML)
+    //Вивід значення поля знайденого елементу
+    // console.log('Поле classList:', itemsDiv.classList)
+    // console.log('Поле id:', itemsDiv.id)
+    // console.log('Поле clientWidth:', itemsDiv.clientWidth)
+    // console.log('Поле innerHTML:', itemsDiv.innerHTML)
 
-    //додавання тексту в блок 
-    //itemsDiv.innerText  =   'Перший Програмно доданий текст '
-    //itemsDiv.innerText  +=   'Другий Програмно доданий текст '
-    //додвання відформатованого HTML коду в блок 
-    //itemsDiv.innerHTML = '<h1>Відформатований HTML код</h1>'
+    //Додавання тексту в блок
+    // itemsDiv.innerHTML = 'Перший Програмно доданий текст'
+    // // itemsDiv.innerHTML = 'Другий Програмно доданий текст'
+    // itemsDiv.innerHTML += 'Другий Програмно доданий текст'
+    //Додавання відформатованого HTML коду в блок
+    // itemsDiv.innerHTML = '<div  class = item></div>'
+    // itemsDiv.innerHTML += '<div  class = "item"></div>'
+    // itemsDiv.innerHTML += '<div  class = "item"></div>'
+    // itemsDiv.innerHTML += '<div  class = "item"></div>'
+    // itemsDiv.innerHTML += '<div  class = "item"></div>'
+    for(let i = 0; i < 100; i++) {
+        itemsDiv.innerHTML += '<div class="item"></div>'
+    }
+
 } else {
+    //Вивід знайденого елементу
     console.log('Блок товарів не знайдено')
 }
-    //вивід
-    //console.log(itemsDiv)
